6958081c8035dc2374fa74ace740a2a22e5405de
annececiledunand <44811434+annececiledunand@users.noreply.github.com>
2025-06-06 14:29:01 +0200

    split main functions into src repo

--- main.py
+++ main.py
@@ -5,19 +5,13 @@ from typing import Annotated
 import typer
 
 from eutils_retrieval.api import NCBIDatabase
-from eutils_retrieval.search import ArticleIds
+from retrieval import ncbi_article_retrieval
 from src.config import (
     UROLOGY_INDICATORS_FLAT,
     HEMOSTATIC_DEVICES_FLAT,
     HEMOSTATIC_DEVICES_MINI_FLAT,
     UROLOGY_INDICATORS_MINI_FLAT,
 )
-from src.eutils_retrieval.query import create_e_queries
-from eutils_retrieval.cross_database_search import merge_article_ids, ncbi_search_and_fetch
-import json
-import time
-from loguru import logger
-
 
 SUBMISSION_RESULTS_FOLDER = Path(__file__).parent / "submission_results"
 
@@ -55,6 +49,7 @@ def main(
         DbNameArg, typer.Option(help="Dbs to call for search. Default to all")
     ] = DbNameArg.ALL,
 ):
+    """Typer method to allow cli run for `ncbi_article_retrieval`"""
     db = DB_NAME_MAPPING[db_name]
     # Use mini to choose a small sample of the real data
     devices_indicators = (HEMOSTATIC_DEVICES_FLAT, UROLOGY_INDICATORS_FLAT)
@@ -63,34 +58,13 @@ def main(
 
     # Check that the folder exists, or creates it
     SUBMISSION_RESULTS_FOLDER.mkdir(exist_ok=True)
-
-    start = time.time()
-    # 1. determine all queries that corresponds to devices & indicators
-    queries = create_e_queries(
-        *devices_indicators,
+    ncbi_article_retrieval(
+        devices_indicators=devices_indicators,
         year_bounds=(start_year, end_year),
+        db=db,
+        output_folder=SUBMISSION_RESULTS_FOLDER,
+        store_intermediate_results=intermediate,
     )
-
-    # 2. Search all articles and fetch summary across databases
-    all_article_ids = ncbi_search_and_fetch(
-        queries, db=db, folder=SUBMISSION_RESULTS_FOLDER if intermediate else None
-    )
-
-    # 3. Deduplicates article records
-    merged_results = merge_article_ids(all_article_ids)
-    logger.success(f"Found {len(merged_results)} total results, took {time.time() - start} seconds")
-
-    # 4. Store results into a json file
-    _store_results(merged_results, SUBMISSION_RESULTS_FOLDER)
-
-
-def _store_results(results: list[ArticleIds], folder: Path):
-    """Write all article ids into a JSON file inside `folder`"""
-    result_file_path = folder / "retrieved_ids.json"
-
-    logger.info(f"Writing results into {result_file_path}")
-    with result_file_path.open("w") as json_writer:
-        json.dump(results, json_writer, indent=4)
 
 
 if __name__ == "__main__":
