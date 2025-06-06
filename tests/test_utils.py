c1e1740accba1957e8d1433b23689b05cce7b1e5
annececiledunand <44811434+annececiledunand@users.noreply.github.com>
2025-06-06 21:53:36 +0200

    fix (coverage): utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,9 @@
-from src.utils import flatten_dict_to_list
+import json
+from pathlib import Path
+
+import pytest
+
+from src.utils import flatten_dict_to_list, store_data_as_json
 
 
 def test_flatten_dict_to_list():
@@ -11,3 +16,22 @@ def test_flatten_dict_to_list():
 
     result = flatten_dict_to_list(dict_to_flatten)
     assert result == expected_flattened
+
+
+def test_store_data_as_json(tmp_path: Path):
+    file_path = tmp_path / "file.json"
+    data = {"bonjour": ["1", "2"]}
+
+    assert not file_path.exists()
+    store_data_as_json(data, file_path)
+    with file_path.open() as reader:
+        result = json.load(reader)
+        assert result == data
+
+
+def test_store_data_as_json_error(tmp_path: Path):
+    file_path = tmp_path / "file.txt"
+    data = {"bonjour": ["1", "2"]}
+
+    with pytest.raises(ValueError, match="should be of json extension"):
+        store_data_as_json(data, file_path)
